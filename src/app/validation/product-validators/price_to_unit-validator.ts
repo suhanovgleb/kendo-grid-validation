60787eda6442624fc39a887161e1daab9715efeb
Gleb Suhanov <suhanovgleb@inbox.ru>
2018-12-14 21:35:22 +0300

    finish moving to Anugular 7 + bug fix

--- src/app/validation/product-validators/price_to_unit-validator.ts
+++ src/app/validation/product-validators/price_to_unit-validator.ts
@@ -13,21 +13,21 @@ export class PriceToUnitValidator implements IValidator {
         const errorMessage = 'If units are out of stock then price cannot be higher than 30';
         const fieldNames = ['Price', 'Quantity'];
 
-        const isActivated = schema.rowValidators.multiRowValidators.find(
+        const isActivated = schema.rowValidators.singleRowValidators.find(
             validator => validator.name === 'priceToUnitValidator'
-        ).option;
-
-        if (isActivated) {
-            for (const item of items) {
-                if (item.hasOwnProperty('Price') && item.hasOwnProperty('Quantity')) {
-                    if (item.Price > 30 && item.Quantity === 0) {
-                        const errorInfo = new ErrorInfo(errorName, errorMessage, this.validatorType);
-                        errors.push(new ValidationError(errorInfo, item, fieldNames));
+        );
+        if (isActivated !== undefined) {
+            if (isActivated.option) {
+                for (const item of items) {
+                    if (item.hasOwnProperty('Price') && item.hasOwnProperty('Quantity')) {
+                        if (item.Price > 30 && item.Quantity === 0) {
+                            const errorInfo = new ErrorInfo(errorName, errorMessage, this.validatorType);
+                            errors.push(new ValidationError(errorInfo, item, fieldNames));
+                        }
                     }
                 }
             }
+            return errors;
         }
-
-        return errors;
     }
 }
